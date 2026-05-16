27284dc7b0f869e834f762a4dbb5cf1b3a2b5a4a
Sameer kumar <sameerkumar88977@gmail.com>
2026-05-17 01:01:14 +0530

    fixed wildcard route

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use("/api/admin", adminRoutes);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 const PORT = process.env.PORT || 5000;
