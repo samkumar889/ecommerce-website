102ae72170166981232ce56f3b65f8b25d7b47cb
Sameer kumar <sameerkumar88977@gmail.com>
2026-05-17 00:36:58 +0530

    complete server fix

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
-const path = require("path");
+
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
