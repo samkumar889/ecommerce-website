7e77837c9e137dfe3cb3dd8483e2b7c42f19f7b9
Sameer kumar <sameerkumar88977@gmail.com>
2026-05-17 00:57:28 +0530

    fixed star route

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,9 @@ app.use("/api/admin", adminRoutes);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/*", (req, res) =>  {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
